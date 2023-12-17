8e44cf13340ec620802d63fb377d6a5dd0f87579
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2023-12-17 16:36:30 +0530

    Add files via upload

---
+++ 17dec.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#print addition of odd and even numbers
+Eadd=0
+Oadd=0
+for num in range(1,11):
+    if(num%2==0):
+        Eadd=Eadd+num
+    else:
+        Oadd=Oadd+num
+print(Eadd
+      
+print(Oadd)
+30
+25
+#prime number or not
+num=int(input("enter number"))
+for i in range(2,num):
+     if (num%i==0):
+            break
+if(i+1==num):
+    print("num is prime")
+else:
+    print("not prime")
+    
+enter number3
+num is prime
+#print multiplication table of given numbers using the for loop
+val = int(input("enter the number"))
+for num in range(1,11):
+    print(val*num)
+print("end")
+enter the number5
+5
+10
+15
+20
+25
+30
+35
+40
+45
+50
+end
+#multiplication table form 11 to 21
+for value in range(11,21):
+    for num in range(1,11):
+        print (value*num,end='')
+print()
+1122334455667788991101224364860728496108120132639526578911041171301428425670849811212614015304560759010512013515016324864809611212814416017345168851021191361531701836547290108126144162180193857769511413315217119020406080100120140160180200
+
+
+
+
+
+ 
+
